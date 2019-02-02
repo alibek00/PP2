62ccb9a306cc6fa79baacbe35b68641722fd806b
alibek00 <alibek.mukanmediev@gmail.com>
2019-02-02 16:55:43 +0600

    lab2

---
+++ lab2/task1/task1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("write string   ");
+            string str = Console.ReadLine();
+
+            string backwardsGuy = new string(str.Reverse().ToArray());
+            if (str == backwardsGuy)
+            {
+                Console.WriteLine("Yes");
+            }
+            else
+            {
+                Console.WriteLine("No");
+            }
+            Console.ReadKey();
+        }
+    }
+}
